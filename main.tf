9b770ca8fd0be920c0f88fe188bbfbf0a907fa4a
aabirdeb <103592645+aabirdeb@users.noreply.github.com>
2024-01-26 12:51:02 +0530

    Called in root main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "lambda_rolee" {
+  source = "./modules/iam_role"
+}
+
+
+module "example_s3_bucket" {
+  source = "./modules/s3_bucket"
+
+  # You can specify any input variables required by your module here
+  # For example, if your module expects a variable named "bucket_name", you can set it like this:
+  bucket_name = "govos-genai-poc-input-datatiff"
+  
+}
+module "example_s3_bucket2" {
+  source      = "./modules/s3_bucket"
+  bucket_name = "govos-genai-poc-output-textractt"
+}
+
